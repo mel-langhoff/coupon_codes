bf3ebb34b297c5ac3c583888fe56c9f2c0ed9b38
Billy Wallace <wallace.billy1@gmail.com>
2024-04-17 15:29:18 -0600

    Clean up invoice_item and invoice_item_spec

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -8,12 +8,7 @@ class InvoiceItem < ApplicationRecord
 
   enum status: { pending: 0, packaged: 1, shipped: 2 }
 
-  # def invoice_item_total_price
-  #   self.sum { |item| item.quantity * item.unit_price }
-  # end
-
   def unit_price_in_dollars
     self.unit_price / 100.00
   end
-
 end
